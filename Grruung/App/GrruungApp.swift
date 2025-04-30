2eb5bbec6ae3103c951c10124ce001c3a8521767
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-04-30 21:54:18 +0900

    [0.0.Ko1] Grruung 프로젝트 추가

---
+++ Grruung/App/GrruungApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrruungApp.swift
+//  Grruung
+//
+//  Created by NoelMacMini on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct GrruungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
