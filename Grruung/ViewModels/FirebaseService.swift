1a714fc6961d778b44f23e70b6798852d2c56bae
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-05-07 13:46:21 +0900

    Feat: Firebase 설정 작성 초기

---
+++ Grruung/ViewModels/FirebaseService.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseService.swift
+//  Grruung
+//
+//  Created by KimJunsoo on 5/7/25.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseDatabase
+
+// Firebase 관련 작업을 처리하는 서비스 클래스
+class FirebaseService {
+    
+}
