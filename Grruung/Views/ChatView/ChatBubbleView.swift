c9c273a007d2367f10e0f95020a9f434b947cbd4
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-05-07 10:44:48 +0900

    Rename: TestView -> ChatView로 변경 & View 작업

---
+++ Grruung/Views/ChatView/ChatBubbleView.swift
@@ -0,0 +1,14 @@
+//
+//  ChatBubbleView.swift
+//  Grruung
+//
+//  Created by KimJunsoo on 5/7/25.
+//
+
+import SwiftUI
+
+struct ChatBubbleView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
