fb18c7d7b88cbcbdc2fd7966a5a2f146fdd80683
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-05-08 09:03:43 +0900

    Fix: Firestore을 위한 수정

--- Grruung/App/GrruungApp.swift
+++ Grruung/App/GrruungApp.swift
@@ -6,15 +6,22 @@
 //
 
 import SwiftUI
+import Firebase
 import FirebaseCore
+import FirebaseFirestore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        // Firestore 설정
+        let settings = FirestoreSettings()
+        settings.isPersistenceEnabled = true
+        Firestore.firestore().settings = settings
+        
+        return true
+    }
 }
 
 @main
