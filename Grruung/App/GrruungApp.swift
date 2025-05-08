6631ab61761d7806be28e02ac9d79c88e631dd92
Rrpe <43327302+Rrpe@users.noreply.github.com>
2025-05-08 23:15:20 +0900

    [Fix] ChatPet 정상 작동 확인 완료

--- Grruung/App/GrruungApp.swift
+++ Grruung/App/GrruungApp.swift
@@ -9,10 +9,18 @@ import SwiftUI
 import Firebase
 import FirebaseCore
 import FirebaseFirestore
+import FirebaseAppCheck // App Check 추가
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        // ✅ AppCheck Debug Provider 먼저 지정
+#if DEBUG
+        AppCheck.setAppCheckProviderFactory(AppCheckDebugProviderFactory())
+#endif
+        
+        // Firebase 초기화
         FirebaseApp.configure()
         
         // Firestore 설정
---
+++ Grruung/ViewModels/ChatPet/AppCheckService.swift
@@ -0,0 +1,8 @@
+//
+//  AppCheckService.swift
+//  Grruung
+//
+//  Created by KimJunsoo on 5/8/25.
+//
+
+import Foundation
