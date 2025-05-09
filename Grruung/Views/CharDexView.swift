df5c38f8aff332ff805b315fad8539e4a43ef20d
mwpark <hyminwoo0000@gmail.com>
2025-05-09 15:59:29 +0900

    Feat: UI 수정 및 잠금 해제 기능 추가

--- Grruung/Views/CharDexView.swift
+++ Grruung/Views/CharDexView.swift
@@ -8,152 +8,106 @@
 import SwiftUI
 
 struct CharDexView: View {
-    // 최대 도감의 캐릭터 갯수
-    private let maxDexCount: Int = 20
-    // 정렬 상태
+    // 생성 가능한 최대 캐릭터 수
+    private let maxDexCount: Int = 10
+    // 초기 생성 가능한 캐릭터 수
+    @State private var unlockCount: Int = 5
+    // 정렬 타입 변수
     @State private var sortType: SortType = .original
     
-    // 그리드 레이아웃
+    @State private var showingAlert = false
+    
+    // 잠금해제 관련 변수
+    @State private var showingUnlockAlert = false
+    // 잠금 그리드 클릭 위치
+    @State private var selectedLockedIndex: Int? = nil
+
     let columns = [
         GridItem(.flexible(), spacing: 16),
         GridItem(.flexible(), spacing: 16)
     ]
     
-    // 더미 데이터(테스트용)
-    let garaCharacter: [GRCharacter] = [
+    // 임시 데이터(테스트 용)
+    @State private var garaCharacters: [GRCharacter] = [
         GRCharacter(species: "고양이사자", name: "구릉이1", imageName: "hare"),
         GRCharacter(species: "고양이사자", name: "구릉이2", imageName: "hare",
                     birthDate: Calendar.current.date(from: DateComponents(year: 2023, month: 12, day: 25))!),
-        GRCharacter(species: "고양이사자", name: "구릉이3", imageName: "hare", birthDate:Calendar.current.date(from: DateComponents(year: 2010, month: 12, day: 13))!),
+        GRCharacter(species: "고양이사자", name: "구릉이3", imageName: "hare",
+                    birthDate: Calendar.current.date(from: DateComponents(year: 2010, month: 12, day: 13))!),
         GRCharacter(species: "고양이사자", name: "구르릉", imageName: "hare",
-                    birthDate:Calendar.current.date(from: DateComponents(year: 2023, month: 2, day: 13))!),
-        GRCharacter(species: "고양이사자", name: "구릉이4", imageName: "hare"),
-        GRCharacter(species: "고양이사자", name: "구릉이5", imageName: "hare"),
+                    birthDate: Calendar.current.date(from: DateComponents(year: 2023, month: 2, day: 13))!),
+//        GRCharacter(species: "고양이사자", name: "구르릉", imageName: "hare",
+//                    birthDate: Calendar.current.date(from: DateComponents(year: 2000, month: 2, day: 13))!),
     ]
     
-    // 정렬 종류
     enum SortType {
         case original
         case createdAscending
         case createdDescending
         case alphabet
     }
-    // 데이터 정렬 연산 프로퍼티
-    var sortedCharacterList: [GRCharacter] {
+    
+    // 현재 캐릭터 슬롯 정렬 프로퍼티
+    var sortedCharacterSlots: [GRCharacter] {
         switch sortType {
         case .original:
-            return garaCharacter
+            return garaCharacters
         case .createdAscending:
-            return garaCharacter.sorted { $0.birthDate > $1.birthDate }
+            return garaCharacters.sorted { $0.birthDate > $1.birthDate }
         case .createdDescending:
-            return garaCharacter.sorted { $0.birthDate < $1.birthDate }
+            return garaCharacters.sorted { $0.birthDate < $1.birthDate }
         case .alphabet:
-            return garaCharacter.sorted { $0.name.localizedCompare($1.name) == .orderedAscending }
+            return garaCharacters.sorted { $0.name.localizedCompare($1.name) == .orderedAscending }
         }
     }
-    
-    // 전체 캐릭터 슬롯
-    private var characterList: [GRCharacter] {
-        var unlockedCharacters = sortedCharacterList
-        let lockedCount = maxDexCount - unlockedCharacters.count - 1
-        let extraCharacter = GRCharacter(species: "", name: "", imageName: "plus")
+
+    private var characterSlots: [GRCharacter] {
+        var hasCharacters = sortedCharacterSlots
+
+        // 생성 가능한 슬롯 수 = unlockCount - 현재 캐릭터 수
+        let addableCount = max(0, unlockCount - hasCharacters.count)
         
-        if lockedCount > 0 {
-            unlockedCharacters.append(extraCharacter)
-            
-            // Array repeat 썼더니 같은 인스턴스로 인식해서 map으로 변경
-            let lockedCharacters = (0..<lockedCount).map { _ in
-                GRCharacter(species: "", name: "", imageName: "lock.fill")
-            }
-            return unlockedCharacters + lockedCharacters
-        } else if lockedCount == 0 {
-            unlockedCharacters.append(extraCharacter)
-            return unlockedCharacters
-        } else {
-            
+        // "plus" 슬롯 추가
+        let plusCharacters = (0..<addableCount).map { _ in
+            GRCharacter(species: "", name: "", imageName: "plus")
         }
-        return unlockedCharacters
+        
+        // 현재까지 채워진 슬롯 수 = 캐릭터 + plus
+        let filledCount = hasCharacters.count + plusCharacters.count
+        
+        // 나머지 잠금 슬롯 수
+        let lockedCount = max(0, maxDexCount - filledCount)
+        let lockedCharacters = (0..<lockedCount).map { _ in
+            GRCharacter(species: "", name: "", imageName: "lock.fill")
+        }
+
+        return hasCharacters + plusCharacters + lockedCharacters
     }
-    
+
     var body: some View {
         NavigationStack {
             ScrollView {
+                HStack {
+                    Text("\(garaCharacters.count)")
+                        .foregroundStyle(.yellow)
+                    Text("/ \(maxDexCount) 수집")
+                }
+                .frame(maxWidth: 180)
+                .font(.title)
+                .background(alignment: .center, content: {
+                    Capsule()
+                        .fill(Color.brown.opacity(0.5))
+                })
+
                 LazyVGrid(columns: columns, spacing: 16) {
-                    ForEach(characterList, id: \.id) { character in
+                    ForEach(Array(characterSlots.enumerated()), id: \.element.id) { index, character in
                         if character.imageName == "lock.fill" {
-                            // 잠긴 슬롯
-                            VStack {
-                                Image(systemName: character.imageName)
-                                    .scaledToFit()
-                                    .font(.system(size: 60))
-                                    .foregroundStyle(.black)
-                                    .frame(height: 180)
-                                    .frame(maxWidth: .infinity)
-                                    .background(Color.black.opacity(0.5))
-                                    .cornerRadius(10)
-                                    .foregroundColor(.gray)
-                                
-                                Text("잠금")
-                                    .foregroundStyle(.black)
-                                    .bold()
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                
-                                Text(" ")
-                                    .foregroundStyle(.gray)
-                                    .font(.caption)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                            }
-                            .padding(.bottom, 16)
+                            lockSlot(at: index)
                         } else if character.imageName == "plus" {
-                            // 추가할 수 있음을 나타내는 표시
-                            VStack {
-                                Image(systemName: character.imageName)
-                                    .scaledToFit()
-                                    .font(.system(size: 60))
-                                    .foregroundStyle(.black)
-                                    .frame(height: 180)
-                                    .frame(maxWidth: .infinity)
-                                    .background(Color.black.opacity(0.3))
-                                    .cornerRadius(10)
-                                    .foregroundColor(.gray)
-                                
-                                Text(" ")
-                                    .foregroundStyle(.black)
-                                    .bold()
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                
-                                Text(" ")
-                                    .foregroundStyle(.gray)
-                                    .font(.caption)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                            }
-                            .padding(.bottom, 16)
+                            addSlot
                         } else {
-                            // 보유한 캐릭터 슬롯
                             NavigationLink(destination: DetailView(character: character)) {
-                                VStack {
-                                    Image(systemName: character.imageName)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .foregroundStyle(.black)
-                                        .frame(height: 180)
-                                        .frame(maxWidth: .infinity)
-                                        .background(Color.black.opacity(0.2))
-                                        .cornerRadius(10)
-                                        .foregroundColor(.gray)
-                                    
-                                    Text(character.name)
-                                        .foregroundStyle(.black)
-                                        .bold()
-                                        .lineLimit(1)
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                    
-                                    Text("\(calculateAge(character.birthDate)) 살 (\(formatToMonthDay(character.birthDate)) 생)")
-                                        .foregroundStyle(.gray)
-                                        .font(.caption)
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                }
-                                .padding(.bottom, 16)
+                                characterSlot(character)
                             }
                         }
                     }
@@ -169,39 +123,121 @@ struct CharDexView: View {
                         } label: {
                             Label("기본", systemImage: sortType == .original ? "checkmark" : "")
                         }
-                        
+
                         Button {
                             sortType = .alphabet
                         } label: {
                             Label("가나다 순", systemImage: sortType == .alphabet ? "checkmark" : "")
                         }
-                        
+
                         Button {
                             sortType = .createdAscending
                         } label: {
                             Label("생성 순 ↑", systemImage: sortType == .createdAscending ? "checkmark" : "")
                         }
-                        
+
                         Button {
                             sortType = .createdDescending
                         } label: {
                             Label("생성 순 ↓", systemImage: sortType == .createdDescending ? "checkmark" : "")
                         }
-                        
+
                     } label: {
                         Label("정렬", systemImage: "arrow.up.arrow.down")
                     }
                 }
             }
+            .alert("슬롯을 해제합니다.", isPresented: $showingUnlockAlert) {
+                Button("해제", role: .destructive) {
+                    if unlockCount < maxDexCount {
+                        unlockCount += 1
+                    }
+                }
+                Button("취소", role: .cancel) {}
+            }
+            .alert("슬롯을 해제하면 더 많은 캐릭터를 추가할 수 있습니다.", isPresented: $showingAlert) {
+                Button("확인", role: .destructive) {
+
+                }
+                Button("취소", role: .cancel) {}
+            }
+            .onAppear {
+                if unlockCount == garaCharacters.count {
+                    showingAlert = true
+                }
+            }
         }
     }
     
+    // 캐릭터 슬롯
+    private func characterSlot(_ character: GRCharacter) -> some View {
+        VStack(alignment: .center) {
+            Image(systemName: character.imageName)
+                .resizable()
+                .frame(width: 100, height: 100, alignment: .center)
+                .aspectRatio(contentMode: .fit)
+                .foregroundStyle(.black)
+
+            Text(character.name)
+                .foregroundStyle(.black)
+                .bold()
+                .lineLimit(1)
+                .frame(maxWidth: .infinity)
+
+            Text("\(calculateAge(character.birthDate)) 살 (\(formatToMonthDay(character.birthDate)) 생)")
+                .foregroundStyle(.gray)
+                .font(.caption)
+                .frame(maxWidth: .infinity)
+        }
+        .frame(height: 180)
+        .frame(maxWidth: .infinity)
+        .background(Color.brown.opacity(0.5))
+        .cornerRadius(20)
+        .foregroundColor(.gray)
+        .padding(.bottom, 16)
+    }
+    
+    // 잠겨있는 슬롯
+    private func lockSlot(at index: Int) -> some View {
+        Button {
+            selectedLockedIndex = index
+            showingUnlockAlert = true
+        } label: {
+            VStack {
+                Image(systemName: "lock.fill")
+                    .scaledToFit()
+                    .font(.system(size: 60))
+                    .foregroundStyle(.black)
+                    .frame(height: 180)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.black.opacity(0.5))
+                    .cornerRadius(20)
+                    .foregroundColor(.gray)
+            }
+            .padding(.bottom, 16)
+        }
+        .buttonStyle(.plain)
+    }
+    
+    // 추가할 수 있는 슬롯(남은 슬롯 표시)
+    private var addSlot: some View {
+        VStack {
+            Image(systemName: "plus")
+                .scaledToFit()
+                .font(.system(size: 60))
+                .frame(height: 180)
+                .frame(maxWidth: .infinity)
+                .background(Color.brown.opacity(0.5))
+                .cornerRadius(20)
+        }
+        .padding(.bottom, 16)
+    }
 }
 
-// 임시 디테일 뷰(테스트 용)
+// 임시 디테일 뷰
 struct DetailView: View {
     var character: GRCharacter
-    
+
     var body: some View {
         VStack {
             Image(systemName: character.imageName)
@@ -210,15 +246,15 @@ struct DetailView: View {
                 .frame(height: 200)
                 .frame(maxWidth: .infinity)
                 .padding()
-            
+
             Text(character.name)
                 .font(.largeTitle)
                 .bold()
-            
+
             Text(character.species)
                 .font(.title)
                 .foregroundStyle(.gray)
-            
+
             Spacer()
         }
         .navigationTitle(character.name)
@@ -226,26 +262,25 @@ struct DetailView: View {
     }
 }
 
-// Date타입을 00월 00일 포맷으로 리턴하는 함수
+// 날짜 -> 00월 00일 포맷으로 변경 함수
 func formatToMonthDay(_ date: Date) -> String {
     let formatter = DateFormatter()
     formatter.dateFormat = "MM월 dd일"
     return formatter.string(from: date)
 }
 
-// 나이를 계산하는 함수
+// 나이 계산 함수
 func calculateAge(_ birthDate: Date) -> Int {
     let calendar = Calendar.current
     let now = Date()
-    
+
     let age = calendar.dateComponents([.year], from: birthDate, to: now).year ?? 0
-    
-    // 생일이 안 지났으면 1살 빼기
+
     if let birthdayThisYear = calendar.date(bySetting: .year, value: calendar.component(.year, from: now), of: birthDate),
        now < birthdayThisYear {
         return age - 1
     }
-    
+
     return age
 }
 
