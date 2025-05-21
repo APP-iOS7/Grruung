9fdd7d5cf46d4d71039b80742a4cb6e6acdb2d9a
NO SEONGGYEONG <no8371@gmail.com>
2025-05-21 15:53:57 +0900

    [Feat]성장 단계 모델 및 서비스 추가, 캐릭터 상세 뷰에 성장 단계 로딩 기능 구현

--- Grruung/Views/TestView/NoTestView.swift
+++ Grruung/Views/TestView/NoTestView.swift
@@ -8,10 +8,17 @@
 import SwiftUI
 
 struct NoTestView: View {
+    
+    let charUUID: String = "CF6NXxcH5HgGjzVE0nVE"
+    
     var body: some View {
         NavigationStack {
-            NavigationLink(destination: CharacterDetailView(characterUUID: "CF6NXxcH5HgGjzVE0nVE")) { // 임시 더미 characterUUID
+            NavigationLink(destination: CharacterDetailView(characterUUID: charUUID)) { // 임시 더미 characterUUID
                 Text("Go to Character Detail")
+            }
+            
+            NavigationLink(destination: WriteStoryView(currentMode: .create, characterUUID: charUUID)) {
+                Text("Go to WriteStory Create")
             }
         }
     }
