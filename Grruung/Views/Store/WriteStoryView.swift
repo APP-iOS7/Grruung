9597ff42716449d8459acbee1e227c9246b84bbb
837100 <shtjdrud0830@naver.com>
2025-06-12 11:46:38 +0900

    [Refactor] WriteStoryView: UI 통일 하도록 배경 수정

--- Grruung/Views/Store/WriteStoryView.swift
+++ Grruung/Views/Store/WriteStoryView.swift
@@ -116,15 +116,12 @@ struct WriteStoryView: View {
                 .padding(.bottom, 30)
             }
         }
-//        .padding(.bottom, 48) // 하단 여백 추가
         .scrollContentBackground(.hidden)
         .background(
             LinearGradient(colors: [
                 Color(GRColor.mainColor1_1),
                 Color(GRColor.mainColor1_2),
-                Color(.systemBackground)
-            ],
-                           startPoint: .top, endPoint: .bottom)
+            ], startPoint: .top, endPoint: .bottom)
         )
         .onTapGesture {
             isTextEditorFocused = false
